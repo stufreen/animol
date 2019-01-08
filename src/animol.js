25f20c7f5314eb770316accd3c6e43d3498ab067
Stu <stu@pilotinteractive.ca>
2019-01-08 12:03:40 -0500

    Refactor easing and interpolation

--- src/animol.js
+++ src/animol.js
@@ -1,22 +1,22 @@
-import { interpolate, interpolateColor, interpolateTransform } from './interpolate';
+import {
+  interpolate,
+  calculateVal,
+  calculateColor,
+  calculateTransform,
+} from './interpolate';
 import { buildFromToList } from './transformList';
 import { Easing } from './easing';
 
 export { Easing } from './easing';
 
-export const css = (
-  element,
-  duration,
-  from = {},
-  to = {},
+export const ease = (
+  callback = () => {},
+  duration = 0,
   easingFunc = Easing.linear,
   delay = 0,
 ) => new Promise((resolve) => {
   let startTime;
   let endTime;
-  const fromToList = buildFromToList(element, from, to);
-
-  console.log(fromToList);
 
   const step = (timestamp) => {
     if (typeof startTime === 'undefined') {
@@ -25,42 +25,36 @@ export const css = (
     } else if (timestamp >= endTime) {
       resolve(); // Done the animation
     } else if (timestamp >= startTime) {
-      fromToList.forEach((item) => {
-        if (item.unit === 'color') {
-          const interpolated = interpolateColor(
-            startTime,
-            endTime,
-            timestamp,
-            item.fromVal,
-            item.toVal,
-            easingFunc,
-          );
-          element.style[item.key] = `rgba(${interpolated.red}, ${interpolated.green}, ${interpolated.blue}, ${interpolated.alpha})`;
-        } else if (item.key === 'transform') {
-          const interpolated = interpolateTransform(
-            startTime,
-            endTime,
-            timestamp,
-            item.fromVal,
-            item.toVal,
-            easingFunc,
-          );
-          element.style.transform = interpolated;
-        } else {
-          const interpolated = interpolate(
-            startTime,
-            endTime,
-            timestamp,
-            item.fromVal,
-            item.toVal,
-            easingFunc,
-          );
-          element.style[item.key] = `${interpolated}${item.unit}`;
-        }
-      });
+      callback(interpolate(startTime, endTime, timestamp, easingFunc));
     }
     window.requestAnimationFrame(step);
   };
-
   window.requestAnimationFrame(step);
 });
+
+export const css = (
+  element,
+  duration,
+  from = {},
+  to = {},
+  easingFunc = Easing.linear,
+  delay = 0,
+) => {
+  const fromToList = buildFromToList(element, from, to);
+  const callback = (progress) => {
+    fromToList.forEach((item) => {
+      if (item.unit === 'color') {
+        const newColor = calculateColor(item.fromVal, item.toVal, progress);
+        element.style[item.key] = `rgba(${newColor.red}, ${newColor.green}, ${newColor.blue}, ${newColor.alpha})`;
+      } else if (item.key === 'transform') {
+        const newTransform = calculateTransform(item.fromVal, item.toVal, progress);
+        element.style.transform = newTransform;
+      } else {
+        const newVal = calculateVal(item.fromVal, item.toVal, progress);
+        element.style[item.key] = `${newVal}${item.unit}`;
+      }
+    });
+  };
+
+  return ease(callback, duration, easingFunc, delay);
+};
--- src/interpolate.js
+++ src/interpolate.js
@@ -2,65 +2,32 @@ export const interpolate = (
   startTime,
   endTime,
   currentTime,
-  startVal,
-  endVal,
   easingFunction,
 ) => {
-  const distance = endVal - startVal;
   const pctElapsed = Math.max(0, (currentTime - startTime) / (endTime - startTime));
-  return startVal + easingFunction(pctElapsed) * distance;
+  return easingFunction(pctElapsed);
 };
 
-export const interpolateColor = (
-  startTime,
-  endTime,
-  currentTime,
-  startVal,
-  endVal,
-  easingFunction,
-) => ({
-  red: interpolate(startTime, endTime, currentTime, startVal.red, endVal.red, easingFunction),
-  blue: interpolate(startTime, endTime, currentTime, startVal.blue, endVal.blue, easingFunction),
-  green: interpolate(
-    startTime,
-    endTime,
-    currentTime,
-    startVal.green,
-    endVal.green,
-    easingFunction,
-  ),
-  alpha: interpolate(
-    startTime,
-    endTime,
-    currentTime,
-    startVal.alpha,
-    endVal.alpha,
-    easingFunction,
-  ),
+export const calculateVal = (startVal, endVal, progress) => {
+  const distance = endVal - startVal;
+  return startVal + (progress * distance);
+};
+
+export const calculateColor = (startVal, endVal, progress) => ({
+  red: calculateVal(startVal.red, endVal.red, progress),
+  green: calculateVal(startVal.green, endVal.green, progress),
+  blue: calculateVal(startVal.blue, endVal.blue, progress),
+  alpha: calculateVal(startVal.alpha, endVal.alpha, progress),
 });
 
 // Interpolate two transform lists and build up a "transform" string
 // TO DO: Allow transforms with commas, e.g. translate(20px, 30px)
-export const interpolateTransform = (
-  startTime,
-  endTime,
-  currentTime,
-  startTransformList,
-  endTransformList,
-  easingFunction,
-) => {
+export const calculateTransform = (startTransformList, endTransformList, progress) => {
   const transforms = startTransformList.reduce(
     (accumulator, { key, val: startVal, unit }, index) => {
       const endVal = endTransformList[index].val;
-      const interpolatedVal = interpolate(
-        startTime,
-        endTime,
-        currentTime,
-        startVal,
-        endVal,
-        easingFunction,
-      );
-      return [...accumulator, `${key}(${interpolatedVal}${unit})`];
+      const newVal = calculateVal(startVal, endVal, progress);
+      return [...accumulator, `${key}(${newVal}${unit})`];
     }, [],
   );
   return transforms.join(' ');
