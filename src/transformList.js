da40a337fdc9eb035f3163217365de13dab3e5ef
Stu Freen <stuart.freen@rangle.io>
2019-01-24 14:28:26 -0500

    Refactor transform list to remove irrelevant transforms

--- src/transformList.js
+++ src/transformList.js
@@ -6,7 +6,6 @@ import {
 } from './parseCss.js';
 import { decomposeTransformMatrix3D, matrix2DTo3D } from './matrix.js';
 import { convenience } from './convenience.js';
-import { roundTo } from './math.js';
 
 export var IDENTITY = {
   translateX: { unit: 'px', val: 0 },
@@ -48,24 +47,6 @@ function inferUnitVal(key, element) {
   };
 }
 
-function defaultToInferred(inferredTransforms, transformList) {
-  return Object.keys(inferredTransforms).reduce((accumulator, tKey) => {
-    if (typeof transformList[tKey] === 'undefined') {
-      accumulator[tKey] = {
-        unit: inferredTransforms[tKey].unit,
-        val: inferredTransforms[tKey].val
-      };
-    } else {
-      var transform = getUnitVal(tKey, transformList);
-      accumulator[tKey] = {
-        unit: transform.unit,
-        val: transform.val
-      };
-    }
-    return accumulator;
-  }, {});
-}
-
 function getUnitVal(key, styleObj) {
   styleObj = styleObj || {};
 
@@ -91,27 +72,6 @@ function getUnitVal(key, styleObj) {
   return { unit: unit, val: val };
 }
 
-function simplifyTransformLists(tFrom, tTo) {
-  var fromFixed = {};
-  var toFixed = {};
-
-  Object.keys(IDENTITY).forEach((tKey) => {
-    if (IDENTITY[tKey].val !== roundTo(tFrom[tKey].val, 3)
-      || IDENTITY[tKey].unit !== tFrom[tKey].unit
-      || IDENTITY[tKey].val !== roundTo(tTo[tKey].val, 3)
-      || IDENTITY[tKey].unit !== tTo[tKey].unit) {
-      var reconciled = reconcileUnits(tFrom[tKey], tTo[tKey]);
-      fromFixed[tKey] = reconciled.from;
-      toFixed[tKey] = reconciled.to;
-    }
-  });
-
-  return {
-    from: fromFixed,
-    to: toFixed
-  };
-}
-
 function reconcileUnits(fromStyle, toStyle) {
   if (fromStyle.unit !== toStyle.unit) {
     if (fromStyle.val === 0) {
@@ -129,10 +89,34 @@ function reconcileUnits(fromStyle, toStyle) {
 }
 
 function buildTransformFromToList(el, from, to) {
+  from = convenience(from);
+  to = convenience(to);
+
   var inferredTransforms = inferTransforms(el);
-  var transformFrom = defaultToInferred(inferredTransforms, convenience(from));
-  var transformTo = defaultToInferred(inferredTransforms, convenience(to));
-  return simplifyTransformLists(transformFrom, transformTo);
+  var transformFrom = {};
+  var transformTo = {};
+
+  // Iterate through the "from" keys, adding matching "to" values if possible
+  Object.keys(from).forEach(function (key) {
+    var transform = typeof to[key] === 'undefined' ? inferredTransforms[key] : getUnitVal(key, to);
+    transform = reconcileUnits(getUnitVal(key, from), transform);
+    transformFrom[key] = { unit: transform.from.unit, val: transform.from.val };
+    transformTo[key] = { unit: transform.to.unit, val: transform.to.val };
+  });
+
+  // Iterate through the "to" keys which did not have "from" values, inferring the "from" vals
+  Object.keys(to).forEach(function (key) {
+    if (typeof from[key] === 'undefined') {
+      var transform = reconcileUnits(inferredTransforms[key], getUnitVal(key, to));
+      transformFrom[key] = { unit: transform.from.unit, val: transform.from.val };
+      transformTo[key] = { unit: transform.to.unit, val: transform.to.val };
+    }
+  });
+
+  return {
+    from: transformFrom,
+    to: transformTo
+  };
 }
 
 export const buildFromToList = (el, from, to) => {
