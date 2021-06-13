86b88636408a34f510c0074717a96523ad8166b0
Stu Freen <stufreen@gmail.com>
2021-06-13 08:58:52 -0400

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     library: "animol",
     libraryTarget: "umd",
+    globalObject: 'this',
   },
   devServer: {
     contentBase: path.resolve(__dirname, "demo"),
